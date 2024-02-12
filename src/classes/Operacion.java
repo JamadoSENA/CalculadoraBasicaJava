008606626c720a326cee2f3ff2f160dfd1a14542
JamadoSENA <J.amadosena@gmail.com>
2024-02-12 08:45:29 -0500

    Avances clase

--- src/classes/Operacion.java
+++ src/classes/Operacion.java
@@ -1,5 +1,7 @@
 package classes;
 
+import java.util.Scanner;
+
 public class Operacion {
 
 		// TODO Auto-generated method stub
@@ -7,8 +9,10 @@ public class Operacion {
 		int numB; 
 		double resultado;
 		
+		Scanner lapiz = new Scanner (System.in);
+		
 		public Operacion () {
-			
+		super();	
 		}
 
 		public int getNumA() {
@@ -34,6 +38,78 @@ public class Operacion {
 		public void setResultado(double resultado) {
 			this.resultado = resultado;
 		}
+		
+		public void Suma() {
+			 
+			 System.out.println("---Suma---");
+			 System.out.println("Ingrese un numero A: ");
+			 this.numA = lapiz.nextInt();
+			 System.out.println("Ingrese un numero B: ");
+			 this.numB = lapiz.nextInt();
+			 this.setResultado(this.getNumA() + this.getNumB());
+			 System.out.println("El resultado es igual a: " + this.getResultado());
+				 
+	    }
+		
+		public void Resta() {
+			 
+			 System.out.println("---Resta---");
+			 System.out.println("Ingrese un numero A: ");
+			 this.numA = lapiz.nextInt();
+			 System.out.println("Ingrese un numero B: ");
+			 this.numB = lapiz.nextInt();
+			 this.setResultado(this.getNumA() - this.getNumB());
+			 System.out.println("El resultado es igual a: " + this.getResultado());
+				 
+			 }
+		
+		public void Multiplicacion() {
+			 
+			 System.out.println("---Multiplicacion---");
+			 System.out.println("Ingrese un numero A: ");
+			 this.numA = lapiz.nextInt();
+			 System.out.println("Ingrese un numero B: ");
+			 this.numB = lapiz.nextInt();
+			 this.setResultado(this.getNumA() * this.getNumB());
+			 System.out.println("El resultado es igual a: " + this.getResultado());
+				 
+			 }
+		
+		public void Division_() {
+			 
+			 System.out.println("---Division---");
+			 System.out.println("Ingrese un numero A: ");
+			 this.numA = lapiz.nextInt();
+			 System.out.println("Ingrese un numero B: ");
+			 this.numB = lapiz.nextInt();
+			 if(this.getNumB() == 0) {
+				 System.out.println("Error, no se puede dividir por cero");
+			 }else if(this.getNumB() > 0){
+				 this.setResultado(this.getNumA() / this.getNumB());
+				 System.out.println("El resultado es igual a: " + this.getResultado());
+			 }}
+			 
+	     public void Porcentaje_() {
+				 
+			 System.out.println("---Porcentaje---");
+			 System.out.println("Ingrese el numero total: ");
+			 this.numA = lapiz.nextInt();
+			 System.out.println("Ingrese el numero del porcentaje a obtener: ");
+			 this.numB = lapiz.nextInt();
+			 this.setResultado((this.getNumB()*100)/this.getNumA());
+			 System.out.println("El resultado es igual a: " + this.getResultado());
+					 
+			 }
+	     
+	     public void Raiz_() {
+	    	 
+	    	 System.out.println("---Raiz---");
+	    	 System.out.println("Ingrese el subradical: ");
+	    	 this.numA = lapiz.nextInt();
+	    	 this.setResultado(Math.sqrt(getNumA()));
+	    	 System.out.println("El resultado es igual a: " + this.getResultado());
+	    		 
+	    	 }
 		
 		
 
