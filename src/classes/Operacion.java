d64089cd83835e3a7d51f5cb82ded2c95bd189f0
JamadoSENA <j.amadosena@gmail.com>
2024-02-13 22:02:36 -0500

    Calculadora Basica v.2

--- src/classes/Operacion.java
+++ src/classes/Operacion.java
@@ -3,114 +3,89 @@ package classes;
 import java.util.Scanner;
 
 public class Operacion {
+    private int numA;
+    private int numB;
+    private double resultado;
 
-		// TODO Auto-generated method stub
-		int numA; 
-		int numB; 
-		double resultado;
-		
-		Scanner lapiz = new Scanner (System.in);
-		
-		public Operacion () {
-		super();	
-		}
+    Scanner lapiz = new Scanner(System.in);
 
-		public int getNumA() {
-			return numA;
-		}
+    public Operacion() {
+        super();
+    }
 
-		public void setNumA(int numA) {
-			this.numA = numA;
-		}
+    public int getNumA() {
+        return numA;
+    }
 
-		public int getNumB() {
-			return numB;
-		}
+    public void setNumA(int numA) {
+        this.numA = numA;
+    }
 
-		public void setNumB(int numB) {
-			this.numB = numB;
-		}
+    public int getNumB() {
+        return numB;
+    }
 
-		public double getResultado() {
-			return resultado;
-		}
+    public void setNumB(int numB) {
+        this.numB = numB;
+    }
 
-		public void setResultado(double resultado) {
-			this.resultado = resultado;
-		}
-		
-		public void Suma() {
-			 
-			 System.out.println("---Suma---");
-			 System.out.println("Ingrese un numero A: ");
-			 this.numA = lapiz.nextInt();
-			 System.out.println("Ingrese un numero B: ");
-			 this.numB = lapiz.nextInt();
-			 this.setResultado(this.getNumA() + this.getNumB());
-			 System.out.println("El resultado es igual a: " + this.getResultado());
-				 
-	    }
-		
-		public void Resta() {
-			 
-			 System.out.println("---Resta---");
-			 System.out.println("Ingrese un numero A: ");
-			 this.numA = lapiz.nextInt();
-			 System.out.println("Ingrese un numero B: ");
-			 this.numB = lapiz.nextInt();
-			 this.setResultado(this.getNumA() - this.getNumB());
-			 System.out.println("El resultado es igual a: " + this.getResultado());
-				 
-			 }
-		
-		public void Multiplicacion() {
-			 
-			 System.out.println("---Multiplicacion---");
-			 System.out.println("Ingrese un numero A: ");
-			 this.numA = lapiz.nextInt();
-			 System.out.println("Ingrese un numero B: ");
-			 this.numB = lapiz.nextInt();
-			 this.setResultado(this.getNumA() * this.getNumB());
-			 System.out.println("El resultado es igual a: " + this.getResultado());
-				 
-			 }
-		
-		public void Division_() {
-			 
-			 System.out.println("---Division---");
-			 System.out.println("Ingrese un numero A: ");
-			 this.numA = lapiz.nextInt();
-			 System.out.println("Ingrese un numero B: ");
-			 this.numB = lapiz.nextInt();
-			 if(this.getNumB() == 0) {
-				 System.out.println("Error, no se puede dividir por cero");
-			 }else if(this.getNumB() > 0){
-				 this.setResultado(this.getNumA() / this.getNumB());
-				 System.out.println("El resultado es igual a: " + this.getResultado());
-			 }}
-			 
-	     public void Porcentaje_() {
-				 
-			 System.out.println("---Porcentaje---");
-			 System.out.println("Ingrese el numero total: ");
-			 this.numA = lapiz.nextInt();
-			 System.out.println("Ingrese el numero del porcentaje a obtener: ");
-			 this.numB = lapiz.nextInt();
-			 this.setResultado((this.getNumB()*100)/this.getNumA());
-			 System.out.println("El resultado es igual a: " + this.getResultado());
-					 
-			 }
-	     
-	     public void Raiz_() {
-	    	 
-	    	 System.out.println("---Raiz---");
-	    	 System.out.println("Ingrese el subradical: ");
-	    	 this.numA = lapiz.nextInt();
-	    	 this.setResultado(Math.sqrt(getNumA()));
-	    	 System.out.println("El resultado es igual a: " + this.getResultado());
-	    		 
-	    	 }
-		
-		
+    public double getResultado() {
+        return resultado;
+    }
 
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public void Suma() {
+        System.out.println("---Suma---");
+        System.out.println("Ingrese un numero A: ");
+        this.numA = lapiz.nextInt();
+        System.out.println("Ingrese un numero B: ");
+        this.numB = lapiz.nextInt();
+        this.setResultado(this.getResultado() + (double) this.getNumA() + this.getNumB());
+        System.out.println("Resultado: " + this.getResultado());
+    }
+
+    public void Resta() {
+        System.out.println("---Resta---");
+        System.out.println("Ingrese un numero B: ");
+        this.numB = lapiz.nextInt();
+        this.setResultado(this.getResultado() - this.getNumB());
+        System.out.println("Resultado: " + this.getResultado());
+    }
+
+    public void Multiplicacion() {
+        System.out.println("---Multiplicacion---");
+        System.out.println("Ingrese un numero B: ");
+        this.numB = lapiz.nextInt();
+        this.setResultado(this.getResultado() * this.getNumB());
+        System.out.println("Resultado: " + this.getResultado());
+    }
+
+    public void Division_() {
+        System.out.println("---Division---");
+        System.out.println("Ingrese un numero B: ");
+        this.numB = lapiz.nextInt();
+        if (this.getNumB() == 0) {
+            System.out.println("Error, no se puede dividir por cero");
+        } else {
+            this.setResultado(this.getResultado() / this.getNumB());
+            System.out.println("Resultado: " + this.getResultado());
+        }
+    }
+
+    public void Porcentaje_() {
+        System.out.println("---Porcentaje---");
+        System.out.println("Ingrese el numero del porcentaje a obtener: ");
+        this.numB = lapiz.nextInt();
+        this.setResultado((this.getNumB() * this.getResultado()) / 100);
+        System.out.println("Resultado: " + this.getResultado());
+    }
+
+    public void Raiz_() {
+        System.out.println("---Raiz---");
+        this.setResultado(Math.sqrt(getResultado()));
+        System.out.println("Resultado: " + this.getResultado());
+    }
 }
--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -1,68 +1,59 @@
 package main;
 import java.util.Scanner;
-import classes.*;
+import classes.Operacion;
 
 public class Calculadora {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int opcion=0;
-		Scanner lapiz = new Scanner (System.in);
-		Addition objSuma = new Addition();
-        Substraction objResta = new Substraction();
-        Multiplication objMultiplicacion = new Multiplication();
-        Division objDivision = new Division();
-        Porcentaje objPorcentaje = new Porcentaje();
-		
-		do {
-			
-			System.out.println("---CALCULADORA BASICA---");
-			System.out.println("Ingrese que operacion desea usar: ");
-			System.out.println("1. Sumar.");
-			System.out.println("2. Resta.");
-			System.out.println("3. Multiplicacion.");
-			System.out.println("4. Division.");
-			System.out.println("5. Raiz Cuadrada.");
-			System.out.println("6. Porcentaje. ");
-			System.out.println("0. Salir.");
-			opcion = lapiz.nextInt();
-			
-			switch(opcion) {
-			
-			case 1:
-				
-			objSuma.Suma();
-			
-			break;
-			
-			case 2:
-				
-		    objResta.Resta();
-		    
-		    break;
-		    
-			case 3:
-				
-			objMultiplicacion.Multiplicacion();
-			
-			break;
-			
-			case 4:
-				
-			objDivision.Division_();
-			
-			break;
-			
-			case 6:
-				
-			objPorcentaje.Porcentaje_();			
-			
-			
-			
-			}
-			
-			
-		}while(opcion!=0);
-	}
+	    public static void main(String[] args) {
+	        Scanner lapiz = new Scanner(System.in);
+	        Operacion objOperacion = new Operacion();
+
+	        int opcion;
+	        do {
+	            mostrarMenu();
+	            opcion = lapiz.nextInt();
+
+	            switch (opcion) {
+	                case 1:
+	                    objOperacion.Suma();
+	                    break;
+
+	                case 2:
+	                    objOperacion.Resta();
+	                    break;
+
+	                case 3:
+	                    objOperacion.Multiplicacion();
+	                    break;
+
+	                case 4:
+	                    objOperacion.Division_();
+	                    break;
+
+	                case 5:
+	                    objOperacion.Raiz_();
+	                    break;
+
+	                case 6:
+	                    objOperacion.Porcentaje_();
+	                    break;
+	            }
+
+	            System.out.println("¿Desea realizar otra operación con el resultado anterior? (1 = Sí, 0 = No)");
+	            opcion = lapiz.nextInt();
+	        } while (opcion == 1);
+	    }
+	    private static void mostrarMenu() {
+	        System.out.println("---CALCULADORA BASICA---");
+	        System.out.println("Ingrese que operación desea usar: ");
+	        System.out.println("1. Sumar.");
+	        System.out.println("2. Resta.");
+	        System.out.println("3. Multiplicación.");
+	        System.out.println("4. División.");
+	        System.out.println("5. Raíz Cuadrada.");
+	        System.out.println("6. Porcentaje.");
+	        System.out.println("0. Salir.");
+	    }
+	    
 
 }
