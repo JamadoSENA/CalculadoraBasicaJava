d2fe872d02342d157d0fa555638c30cc76e85a64
JamadoSENA <j.amadosena@gmail.com>
2024-02-14 08:33:52 -0500

    Calculadora Basica-Cientifica v.3

--- src/classes/Operacion.java
+++ src/classes/Operacion.java
@@ -39,53 +39,201 @@ public class Operacion {
 
     public void Suma() {
         System.out.println("---Suma---");
+        if(this.getResultado()==0) {
         System.out.println("Ingrese un numero A: ");
         this.numA = lapiz.nextInt();
         System.out.println("Ingrese un numero B: ");
         this.numB = lapiz.nextInt();
-        this.setResultado(this.getResultado() + (double) this.getNumA() + this.getNumB());
+        this.setResultado(this.getNumA() + this.getNumB());
         System.out.println("Resultado: " + this.getResultado());
+        }else {
+            System.out.println("Ingrese un numero B: ");
+            this.numB = lapiz.nextInt();
+        	this.setResultado(this.getResultado() + this.getNumB());
+            System.out.println("Resultado: " + this.getResultado());
+        }
+        
     }
 
     public void Resta() {
-        System.out.println("---Resta---");
+    	System.out.println("---Resta---");
+        if(this.getResultado()==0) {
+        System.out.println("Ingrese un numero A: ");
+        this.numA = lapiz.nextInt();
         System.out.println("Ingrese un numero B: ");
         this.numB = lapiz.nextInt();
-        this.setResultado(this.getResultado() - this.getNumB());
+        this.setResultado(this.getNumA() - this.getNumB());
         System.out.println("Resultado: " + this.getResultado());
-    }
-
-    public void Multiplicacion() {
-        System.out.println("---Multiplicacion---");
-        System.out.println("Ingrese un numero B: ");
-        this.numB = lapiz.nextInt();
-        this.setResultado(this.getResultado() * this.getNumB());
-        System.out.println("Resultado: " + this.getResultado());
-    }
-
-    public void Division_() {
-        System.out.println("---Division---");
-        System.out.println("Ingrese un numero B: ");
-        this.numB = lapiz.nextInt();
-        if (this.getNumB() == 0) {
-            System.out.println("Error, no se puede dividir por cero");
-        } else {
-            this.setResultado(this.getResultado() / this.getNumB());
+        }else {
+            System.out.println("Ingrese un numero B: ");
+            this.numB = lapiz.nextInt();
+        	this.setResultado(this.getResultado() - this.getNumB());
             System.out.println("Resultado: " + this.getResultado());
         }
     }
 
+    public void Multiplicacion() {
+    	System.out.println("---Multiplicacion---");
+        if(this.getResultado()==0) {
+        System.out.println("Ingrese un numero A: ");
+        this.numA = lapiz.nextInt();
+        System.out.println("Ingrese un numero B: ");
+        this.numB = lapiz.nextInt();
+        this.setResultado(this.getNumA() * this.getNumB());
+        System.out.println("Resultado: " + this.getResultado());
+        }else {
+            System.out.println("Ingrese un numero B: ");
+            this.numB = lapiz.nextInt();
+        	this.setResultado(this.getResultado() * this.getNumB());
+            System.out.println("Resultado: " + this.getResultado());
+        }
+    }
+
+    public void Division_() {
+    	System.out.println("---Division---");
+        if(this.getResultado()==0) {
+        System.out.println("Ingrese un numero A: ");
+        this.numA = lapiz.nextInt();
+        System.out.println("Ingrese un numero B: ");
+        this.numB = lapiz.nextInt();
+        	if(this.getNumB()==0) {
+        		System.out.println("Error al dividir por 0.");}
+        	    else {
+        	    this.setResultado(this.getNumA() / this.getNumB());
+        	    System.out.println("Resultado: " + this.getResultado());
+        }
+        }else {
+            System.out.println("Ingrese un numero B: ");
+            this.numB = lapiz.nextInt();
+            if(this.getNumB()==0) {
+        		System.out.println("Error al dividir por 0.");}
+        	    else {
+        	    this.setResultado(this.getResultado() * this.getNumB());
+                System.out.println("Resultado: " + this.getResultado());
+        	
+        	    }
+            }
+    }
+
     public void Porcentaje_() {
-        System.out.println("---Porcentaje---");
+    	System.out.println("---Porcentaje---");
+        if(this.getResultado()==0) {
+        System.out.println("Ingrese el numero a obtener el porcentaje: ");
+        this.numA = lapiz.nextInt();
         System.out.println("Ingrese el numero del porcentaje a obtener: ");
         this.numB = lapiz.nextInt();
-        this.setResultado((this.getNumB() * this.getResultado()) / 100);
+        this.setResultado((this.getNumA() * this.getNumB()) / 100);
         System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	System.out.println("Ingrese el numero del porcentaje a obtener: ");
+            this.numB = lapiz.nextInt();
+            this.setResultado((this.getResultado() * this.getNumB()) / 100);
+            System.out.println("Resultado: " + this.getResultado());
+        }
     }
 
     public void Raiz_() {
-        System.out.println("---Raiz---");
-        this.setResultado(Math.sqrt(getResultado()));
+    	System.out.println("---Raiz Cuadrada---");
+        if(this.getResultado()==0) {
+        System.out.println("Ingrese el numero: ");
+        this.numA = lapiz.nextInt();
+        this.setResultado(Math.sqrt(this.getNumA()));
         System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	this.setResultado(Math.sqrt(this.getResultado()));
+            System.out.println("Resultado: " + this.getResultado());
+        }
+        }
+    
+    public void COS() {
+    	System.out.println("---Coseno---");
+        if(this.getResultado()==0) {
+        	System.out.println("Ingrese el numero: ");
+        	this.numA = lapiz.nextInt();
+        	this.setResultado(Math.cos(this.getNumA()));
+            System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	this.setResultado(Math.cos(this.getResultado()));
+            System.out.println("Resultado: " + this.getResultado());
+        }
     }
+    
+    public void SIN() {
+    	System.out.println("---Seno---");
+        if(this.getResultado()==0) {
+        	System.out.println("Ingrese el numero: ");
+        	this.numA = lapiz.nextInt();
+        	this.setResultado(Math.sin(this.getNumA()));
+            System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	this.setResultado(Math.sin(this.getResultado()));
+            System.out.println("Resultado: " + this.getResultado());
+        }
+    }
+    
+    public void TAN() {
+    	System.out.println("---Tangente---");
+        if(this.getResultado()==0) {
+        	System.out.println("Ingrese el numero: ");
+        	this.numA = lapiz.nextInt();
+        	this.setResultado(Math.tan(this.getNumA()));
+            System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	this.setResultado(Math.tan(this.getResultado()));
+            System.out.println("Resultado: " + this.getResultado());
+        }
+        
+    
+    }
+    
+    public void COTAN() {
+    	System.out.println("---Cotangente---");
+        if(this.getResultado()==0) {
+        	System.out.println("Ingrese el numero: ");
+        	this.numA = lapiz.nextInt();
+        	this.setResultado(Math.atan(this.getNumA()));
+            System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	this.setResultado(Math.atan(this.getResultado()));
+            System.out.println("Resultado: " + this.getResultado());
+        }
+    }
+    
+    public void PI() {
+    	System.out.println("---PI---");
+        if(this.getResultado()==0) {
+        	System.out.println("Ingrese el numero: ");
+        	this.numA = lapiz.nextInt();
+        	this.setResultado(Math.PI*(this.getNumA()));
+            System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	this.setResultado(Math.PI*(this.getResultado()));
+            System.out.println("Resultado: " + this.getResultado());
+        }
+    }
+    
+    public void Potencia() {
+    	System.out.println("---Potencia---");
+        if(this.getResultado()==0) {
+        	System.out.println("Ingrese el numero base: ");
+        	this.numA = lapiz.nextInt();
+        	System.out.println("Ingrese el numero exponente: ");
+        	this.numB = lapiz.nextInt();
+        	this.setResultado(Math.pow(this.getNumA(), this.getNumB()));
+            System.out.println("Resultado: " + this.getResultado());
+        }else {
+        	System.out.println("Ingrese el numero exponente: ");
+        	this.numB = lapiz.nextInt();
+        	this.setResultado(Math.pow(this.getResultado(), this.getNumB()));
+            System.out.println("Resultado: " + this.getResultado());
+        }
+    }
+    
+    
+    
+    
+    
+     
+    
+    
 }
--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -15,33 +15,150 @@ public class Calculadora {
 
 	            switch (opcion) {
 	                case 1:
-	                    objOperacion.Suma();
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.Suma();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.Suma();
+	    	            }
 	                    break;
 
 	                case 2:
-	                    objOperacion.Resta();
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.Resta();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.Resta();
+	    	            }
 	                    break;
 
 	                case 3:
-	                    objOperacion.Multiplicacion();
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.Multiplicacion();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.Multiplicacion();
+	    	            }
 	                    break;
 
 	                case 4:
-	                    objOperacion.Division_();
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.Division_();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.Division_();
+	    	            }
 	                    break;
 
 	                case 5:
-	                    objOperacion.Raiz_();
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.Raiz_();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.Raiz_();
+	    	            }
 	                    break;
 
 	                case 6:
-	                    objOperacion.Porcentaje_();
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.Porcentaje_();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.Porcentaje_();
+	    	            }
 	                    break;
+	                    
+	                case 7:
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.COS();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.COS();
+	    	            }
+	    	            
+	                	break;
+	                	
+	                case 8:
+	                	
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.SIN();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.SIN();
+	    	            }
+	    	            
+	                	break;
+	                	
+	                case 9:
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.TAN();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.TAN();
+	    	            }
+	    	            
+	                	break;
+	                	
+	                case 10:
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.COTAN();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.COTAN();
+	    	            }
+	    	            
+	                	break;
+	                	
+	                case 11:
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.PI();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.PI();
+	    	            }
+	    	            
+	                	break;
+	                	
+	                case 12:
+	                	System.out.println("¿Desea realizar otra operación con el resultado anterior? (2 = No, 1 = Sí, 0 = No)");
+	    	            opcion = lapiz.nextInt(); 
+	    	            if(opcion==2) {
+	    	            	objOperacion.setResultado(0);
+	    	            	objOperacion.Potencia();
+	    	            }else if(opcion==1) {
+	    	            	objOperacion.Potencia();
+	    	            }
+	    	            
+	                	break;
+	                	
+	                default:
+	                	System.out.println("No es una opcion, digite otra.");
+	                	
 	            }
 
-	            System.out.println("¿Desea realizar otra operación con el resultado anterior? (1 = Sí, 0 = No)");
-	            opcion = lapiz.nextInt();
-	        } while (opcion == 1);
+	        } while (opcion != 0);
 	    }
 	    private static void mostrarMenu() {
 	        System.out.println("---CALCULADORA BASICA---");
@@ -52,6 +169,12 @@ public class Calculadora {
 	        System.out.println("4. División.");
 	        System.out.println("5. Raíz Cuadrada.");
 	        System.out.println("6. Porcentaje.");
+	        System.out.println("7. Coseno.");
+	        System.out.println("8. Seno.");
+	        System.out.println("9. Tangente.");
+	        System.out.println("10. Cotangente.");
+	        System.out.println("11. π.");
+	        System.out.println("12. Potencia.");
 	        System.out.println("0. Salir.");
 	    }
 	    
